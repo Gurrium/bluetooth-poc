80b7ea0c6d8ce72548aff4666244a5e77bf339e3
gurrium <gurrium@hatena.ne.jp>
2022-02-14 23:09:07 +0900

    day 240

--- bluetooth-poc/ContentView.swift
+++ bluetooth-poc/ContentView.swift
@@ -6,16 +6,64 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
+    @ObservedObject var state: ContentState
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            if (state.isBluetoothEnabled) {
+                Button("Scan") {
+                    state.scanPeripherals()
+                }
+                List(state.discoveredPeripherals.sorted(by: { $0.key.uuidString > $1.key.uuidString }), id: \.key) { peripheral in
+                    Text(peripheral.value.name ?? "?")
+                }
+            } else {
+                Text("Bluetooth is not enabled.")
+                Text("State: \(state.centralManagerState.rawValue)")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(state: ContentState())
+    }
+}
+
+final class ContentState: NSObject, ObservableObject {
+    @Published var centralManagerState = CBManagerState.unknown {
+        didSet {
+            isBluetoothEnabled = centralManagerState == .poweredOn
+        }
+    }
+    @Published var isBluetoothEnabled = false
+    @Published var discoveredPeripherals = [UUID:CBPeripheral]()
+
+    private let centralManager: CBCentralManager
+
+    override init() {
+        centralManager = CBCentralManager()
+
+        super.init()
+
+        centralManager.delegate = self
+    }
+
+    func scanPeripherals() {
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
+}
+
+extension ContentState: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        centralManagerState = central.state
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        discoveredPeripherals[peripheral.identifier] = peripheral
     }
 }
--- bluetooth-poc/bluetooth_pocApp.swift
+++ bluetooth-poc/bluetooth_pocApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct bluetooth_pocApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(state: ContentState())
         }
     }
 }
