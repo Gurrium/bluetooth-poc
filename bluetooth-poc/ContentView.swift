4da9c6b9af2674541b7392199d76d77949bb3d5f
gurrium <gurrium@hatena.ne.jp>
2022-02-14 21:19:31 +0900

    Initial Commit

---
+++ bluetooth-poc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bluetooth-poc
+//
+//  Created by gurrium on 2022/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bluetooth-poc/bluetooth_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetooth_pocApp.swift
+//  bluetooth-poc
+//
+//  Created by gurrium on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct bluetooth_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
